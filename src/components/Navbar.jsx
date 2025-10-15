bbd66af39b27c0542f21929b70f518a434fc6241
Daniel J. Barros <danibarrosj@outlook.com>
2025-10-15 13:22:50 -0400

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -63,7 +63,7 @@ export const Navbar = () => {
           <ThemeToggle />
           <button
             onClick={() => setIsMenuOpen((p) => !p)}
-            className="md:hidden p-2 text-foreground"
+            className="md:hidden p-2 text-foreground ml-4"
             aria-label={isMenuOpen ? "Close Menu" : "Open Menu"}
           >
             {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
